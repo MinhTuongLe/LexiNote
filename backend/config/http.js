5a9302d865145d56832ede0f5b886ef1e40b34de
LeMinhTuong <102809207+MinhTuongLe@users.noreply.github.com>
2026-03-18 02:28:39 +0700

    config: Add initial HTTP server settings and middleware configuration.

--- backend/config/http.js
+++ backend/config/http.js
@@ -30,7 +30,6 @@ module.exports.http = {
     ***************************************************************************/
 
     order: [
-      'corsFix',
       'cookieParser',
       'session',
       'bodyParser',
@@ -40,17 +39,6 @@ module.exports.http = {
       'www',
       'favicon',
     ],
-
-    corsFix: function (req, res, next) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization');
-      
-      if (req.method === 'OPTIONS') {
-        return res.status(200).send();
-      }
-      return next();
-    },
 
 
     /***************************************************************************
